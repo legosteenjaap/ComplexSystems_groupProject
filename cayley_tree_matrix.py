e8c2bc75195992b4f4759b9c204234d2486c6d6c
legosteenjaap <jaap@tettelaar.nl>
2024-04-10 16:57:30 +0200

    Cleaned up the adjacency matrix generation code

---
+++ cayley_tree_matrix.py
@@ -0,0 +1,107 @@
+import numpy as np
+from scipy import sparse
+
+def get_amount_of_nodes(degree, depth):
+    """Calculates the amount of nodes for the cayley tree with the given degree and depth"""
+    if depth < 0:
+        return 0
+    elif depth == 0:
+        return 1 #Single node
+    elif depth == 1:
+        return 1 + degree 
+    else:
+        matrix_size = 1 + degree
+        next_layer_nodes = degree         
+        
+        for i in range(depth - 1):
+            next_layer_nodes = next_layer_nodes * (degree - 1)
+            matrix_size += next_layer_nodes
+        return matrix_size
+
+def fill_non_sparse_matrix(matrix, row_pos, column_pos):
+    """Stores the data on a normal matrix"""
+    matrix[row_pos, column_pos] = 1
+
+class SparseData:
+    """Simple class for storing sparse matrix data"""
+
+    def __init__(self, matrix_size):
+        self.row_positions = np.zeros(matrix_size - 1)
+        self.column_positions = np.zeros(matrix_size - 1)
+        self.index = 0
+    
+    def get_row_positions(self):
+        return self.row_positions
+    
+    def get_column_positions(self):
+        return self.column_positions
+    
+    def get_index(self):
+        return self.index
+    
+    def increment_index(self):
+        self.index +=1
+
+
+def fill_sparse_matrix(sparse_matrix : SparseData, row_pos, column_pos):
+    """Stores the data for the special data structure which can hold sparse matrices"""
+    sparse_matrix.row_positions[sparse_matrix.get_index()] = row_pos
+    sparse_matrix.column_positions[sparse_matrix.get_index()] = column_pos
+    sparse_matrix.increment_index()
+
+def create_matrix(matrix_size, is_sparse):
+    """Initializes cayley tree matrix"""
+
+    if is_sparse:
+        fill_function = fill_sparse_matrix
+
+        matrix = SparseData(matrix_size)
+    else:
+        fill_function = fill_non_sparse_matrix
+
+        matrix = np.zeros((matrix_size, matrix_size))
+
+    return matrix, fill_function
+
+def fill_matrix(matrix, fill_function, degree, depth):
+    """Fills the adjacency matrix with the data for the cayley tree"""
+
+    #Amount of nodes that aren't on the outside of the tree a.k.a non leaf nodes
+    non_leaf_node_amount = get_amount_of_nodes(degree, depth - 1)
+
+    current_column = 1
+    for i in range(non_leaf_node_amount):
+        next_layer_nodes = (degree - 1) if i !=0 else degree
+        
+        for j in range(next_layer_nodes):
+            fill_function(matrix, i, current_column + j)
+        
+        current_column += next_layer_nodes
+    return matrix
+
+def get_bi_directional(matrix):
+    """Returns a symmetric version of the matrix by adding it up with it's transposed version"""
+    return matrix + np.transpose(matrix)
+
+def generate(degree=3, depth=10, is_sparse=False, is_bi_directional=True):
+    """Generates an adjacency matrix for a cayley tree
+        Arguments:
+            degree (int): The degree of all non leaf nodes on the cayley tree
+            depth (int): The distance from the center at which the cayley tree ends and it's leaves generate
+            is_sparse (bool): Records if the cayley tree should be stored in memory with a special data structure for sparse matrices or not
+            is_bi_directional (bool): Records if the matrix should be made symmetric by adding it up with it's transposed variant"""
+    
+    matrix_size = get_amount_of_nodes(degree, depth)
+
+    matrix, fill_function = create_matrix(matrix_size, is_sparse)
+
+    matrix = fill_matrix(matrix, fill_function, degree, depth)
+
+    if is_sparse:
+        data = np.ones(matrix_size - 1)
+        matrix = sparse.coo_matrix((data, (matrix.get_row_positions(), matrix.get_column_positions())), shape=(matrix_size, matrix_size))
+
+    if is_bi_directional:
+        return get_bi_directional(matrix)
+
+    return matrix
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy
 from scipy import sparse
+import cayley_tree_matrix
 import matplotlib.pyplot as plt
 import time
 import os
@@ -9,110 +10,11 @@ import os
 Python script for Complex Systems group project (rivers)    
 """
 
-#this is the directed sparse version
-#(the idear of sparce is that you can calculate bigger sizes matrixes, don't know actualy works)
-def generate_adjacency_matrix_sparse(degree=3, depth=10):
-    #(so when choosing depth of 0 => 1 node with no connections)
-
-    #calc size matrix
-    size_matrix = calc_size_matrix(degree, depth)
-
-    #total amount of nodes that are connected to an other node
-    size_connected_points = calc_size_matrix(degree, depth - 1)
-
-    index_row = np.zeros(size_matrix - 1)
-    index_col = np.zeros(size_matrix - 1)
-    data = np.ones(size_matrix - 1)
-
-    index_counter = 0
-
-    #populate matrix
-    y_cordinates_counter = 1
-    for i in range(size_connected_points):
-        if i == 0:
-            for j in range(degree):
-                index_row[index_counter] = y_cordinates_counter + j
-                index_col[index_counter] = i
-                index_counter += 1
-            y_cordinates_counter += degree
-        else:
-            for j in range(degree - 1):
-                index_row[index_counter] = y_cordinates_counter + j
-                index_col[index_counter] = i
-                index_counter += 1
-            y_cordinates_counter += degree - 1
-                
-    # base_matrix = sparse.coo_matrix((data, (index_row, index_col)), shape=(size_matrix, size_matrix), dtype=np.int8).toarray()
-    # base_matrix = sparse.coo_matrix((data, (index_row, index_col)), shape=(size_matrix, size_matrix)).toarray()
-    base_matrix = sparse.coo_matrix((data, (index_row, index_col)), shape=(size_matrix, size_matrix))
-    return base_matrix
-
-#this is the directed non sparce version
-def generate_adjacency_matrix_np_version(degree=3, depth=15):
-    #(so when choosing depth of 0 => 1 node with no connections)
-
-    #calc size matrix
-    size_matrix = calc_size_matrix(degree, depth)
-
-    base_matrix = np.zeros((size_matrix,size_matrix))
-
-    #total amount of nodes that are connected to an other node
-    size_connected_points = calc_size_matrix(degree, depth - 1)
-
-    #populate matrix
-    y_cordinates_counter = 1
-    for i in range(size_connected_points):
-        if i == 0:
-            for j in range(degree):
-                base_matrix[y_cordinates_counter + j, i] = 1
-            y_cordinates_counter += degree
-        else:
-            for j in range(degree - 1):
-                base_matrix[y_cordinates_counter + j, i] = 1
-            y_cordinates_counter += degree - 1
-                
-    return base_matrix
-
-def generate_bidirectional_matrix_sparse(degree=3, depth=10):
-    matrix_1 = generate_adjacency_matrix_sparse(degree, depth)
-    matrix_1t = np.transpose(matrix_1)
-    return matrix_1 + matrix_1t
-
-def generate_bidirectional_matrix_np_version(degree=3, depth=10):
-    matrix_1 = generate_adjacency_matrix_np_version(degree, depth)
-    matrix_1t = np.transpose(matrix_1)
-    return matrix_1 + matrix_1t
-
-def calc_size_matrix(degree, depth):
-    if depth == 0:
-        return 1  #(origin point)
-
-    elif depth == 1:
-        return 1 + degree
-
-    else:
-        total_value = 1 + degree
-        outer_nodes = degree         
-        
-        for i in range(depth - 1):
-            outer_nodes = outer_nodes * (degree - 1)
-            total_value += outer_nodes
-        return total_value
-
-
-
-
-#just a function that prints a table to terminal, to give you an impression of 
-# the different matrix sizes and different depths
-def show_sizes(max_depth=10):
-    print(f"size \t split=3 \t split=4")
-    for i in range(max_depth+1):
-        print(f"{i} \t {calc_size_matrix(3, i)} \t {calc_size_matrix(4, i)}")
 
 
 def eigenvalues_sparse(degree, depth):
     start_time = time.time()
-    temp_matrix = generate_bidirectional_matrix_sparse(degree=degree, depth=depth)
+    temp_matrix = cayley_tree_matrix.generate(degree=degree, depth=depth, is_sparse=True)
     temp_size_matrix = temp_matrix.shape[0]
     eigenvalues, eigenvectors = scipy.sparse.linalg.eigs(temp_matrix, k=temp_size_matrix-2)
     end_time = time.time()
@@ -123,8 +25,7 @@ def eigenvalues_sparse(degree, depth):
 
 def eigenvalues_non_sparse(degree, depth):
     start_time = time.time()
-    temp_matrix = generate_bidirectional_matrix_np_version(degree=degree, depth=depth)
-    temp_size_matrix = temp_matrix.shape[0]
+    temp_matrix = cayley_tree_matrix.generate(degree=degree, depth=depth)
     eigenvalues, eigenvectors = np.linalg.eig(temp_matrix)
     end_time = time.time()
 
@@ -138,7 +39,7 @@ def calc_and_safe_data(depth_beginning, depth_end, degree):
     for depth in range(depth_beginning, depth_end + 1): #(is +1 correct?)
         #choose which method (sparse or non sparse [nonsparse seems to be faster...])
         eigenvalues, time = eigenvalues_non_sparse(depth=depth, degree=degree)
-        # eigenvalues, time = eigenvalues_sparse(depth=depth, degree=degree)
+        #eigenvalues, time = eigenvalues_sparse(depth=depth, degree=degree)
     
         print(f"current_settings, degree={degree}, depth={depth}")
         print(f"Time passed: {time}")
@@ -174,9 +75,6 @@ if __name__ == "__main__":
     calc_and_safe_data(4, 13, 3)
     calc_and_safe_data(4, 8, 4)
     calc_and_safe_data(4, 5, 5)
-    
-
-
 
     #make plots
     # show matrix adjecancy form
